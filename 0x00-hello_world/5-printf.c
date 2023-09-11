508afcc8286d8f595a45ca27919ee927ab6efef0
SabeloInnocent01 <sabeloinnocent84@gmail.com>
2023-09-11 08:57:05 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main prints out output using printf
+ *
+ * always return (0)
+ *
+ */
+int main(void)
+{
+	printif("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
