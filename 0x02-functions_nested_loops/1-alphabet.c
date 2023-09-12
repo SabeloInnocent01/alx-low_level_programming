2b357cf35ace2133eea8ff357a89c104635c4bf2
SabeloInnocent01 <sabeloinnocent84@gmail.com>
2023-09-12 09:14:19 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lower cases followed by a new line
+ *
+ * Return: (0)
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+
+	return 0;
+}
